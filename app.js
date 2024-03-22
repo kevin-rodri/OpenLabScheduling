ae2c5f26ffd1310d38acab2e0d36c3740080715b
kevin-rodri <kr399880@gmail.com>
2024-03-21 23:24:19 -0400

    added three routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var config = require("./config");
+const mongoose = require("mongoose");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var labHubRouter = require('./routes/labhub');
